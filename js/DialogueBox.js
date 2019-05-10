d1cdde4407530a30b8db5dfb3f13aa0fdedcca3e
Mihai Pure <39124927+MihaiPure@users.noreply.github.com>
2019-05-10 15:19:36 +0300

    Updated dialogue boxes
    added visual improvements
    3 different types of classes implemented

--- js/DialogueBox.js
+++ js/DialogueBox.js
@@ -1,23 +1,21 @@
-class DialogueBox {
+class MonologueBox {
     constructor() {
         this.box = document.createElement("div");
         this.box.id = "dialogue-box";
+        this.box.classList.add("no-options");
+
 
         this.content = document.createElement("div");
         this.content.id = "question";
 
-        this.options = document.createElement("ul");
-        this.options.id = "option-list";
-
         this.box.appendChild(this.content);
-        this.box.appendChild(this.options);
         document.body.appendChild(this.box);
     }
 }
 
-_p = DialogueBox.prototype;
+_m = MonologueBox.prototype;
 
-_p.setQuestion = function (text) {
+_m.setQuestion = function (text) {
     if (this.content.hasChildNodes()) {
         this.content.replaceChild(document.createTextNode(text), this.content.firstChild);
     } else {
@@ -25,8 +23,45 @@ _p.setQuestion = function (text) {
     }
 }
 
-_p.setOptions = function (...options) {
-    console.log(options);
+_m.waitOnInput = function () {
+    window.addEventListener("keyup", function (e) {
+        if (e.keyCode === 32 || e.keyCode === 13) {
+            alert("A mers!");
+            return 1;
+        }
+    });
+}
+
+_m.remove = function () {
+    document.body.removeChild(this.box);
+}
+
+class DialogueBox extends MonologueBox {
+    constructor() {
+        super();
+        this.box.classList.remove("no-options");
+        this.box.classList.add("four-options");
+
+        this.options = document.createElement("ul");
+        this.options.id = "option-list";
+        this.options.classList.add("four-option-list");
+
+        this.box.appendChild(this.options);
+
+        (function nuFacNimic() {
+
+        })();
+    }
+}
+
+_d = DialogueBox.prototype;
+
+_d.setOptions = function (...options) {
+    if (this.options.hasChildNodes()) {
+        while (this.options.firstChild) {
+            this.options.removeChild(this.options.firstChild);
+        }
+    }
     for (let option in options) {
         var newOption = document.createElement("li");
         newOption.classList.add("options");
@@ -34,4 +69,25 @@ _p.setOptions = function (...options) {
 
         this.options.appendChild(newOption);
     }
+}
+
+_d.waitOnInput = function () {
+
+}
+
+class YesNoBox extends DialogueBox {
+    constructor() {
+        super();
+        this.box.classList.remove("four-options");
+        this.box.classList.add("two-options");
+
+        this.options.classList.add("two-option-list");
+    }
+}
+
+_y = YesNoBox.prototype;
+
+_y.setOptions = function (...options) {
+    newOptions = options.slice(0, 2);
+    _d.setOptions.call(this, ...newOptions);
 }
--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 var DEBUGGING = true;
 
 MapLoader.RESOURCES = [
-	{
-		name : "MainMap",
-		itemType : "JSON",
-		url : "Tiled/map/map1.json"
-	},
-	{
-		name : "CastleMap",
-		itemType : "JSON",
-		url : "Tiled/map/castle2.json"
+    {
+        name: "MainMap",
+        itemType: "JSON",
+        url: "Tiled/map/map1.json"
 	},
     {
-        name : "Dungeon",
-        itemType : "JSON",
-        url : "Tiled/map/dungeon.json"
+        name: "CastleMap",
+        itemType: "JSON",
+        url: "Tiled/map/castle2.json"
+	},
+    {
+        name: "Dungeon",
+        itemType: "JSON",
+        url: "Tiled/map/dungeon.json"
     },
 ];
 
 // in case the name of the map resources changes
-const MAIN_MAP = "MainMap", CASTLE_MAP = "CastleMap", DUNGEON = "Dungeon";
+const MAIN_MAP = "MainMap",
+    CASTLE_MAP = "CastleMap",
+    DUNGEON = "Dungeon";
 
 /*
     when this event fires on the mapLoader instance 
@@ -35,33 +37,33 @@ var resourceLoader, canvasManager, mapRenderer, mapLoader, player, movementManag
 
 function init() {
     loadingScreen = new LoadingScreen();
-	if (DEBUGGING) {
-		var stats = new xStats();
-		document.body.appendChild(stats.element);
-	}
-	
-	resourceLoader = new ResourceLoader();
-	canvasManager = CanvasManagerFactory(document.getElementById("gameCanvas"));
-	
-	mapLoader = new MapLoader(resourceLoader);
-    
+    if (DEBUGGING) {
+        var stats = new xStats();
+        document.body.appendChild(stats.element);
+    }
+
+    resourceLoader = new ResourceLoader();
+    canvasManager = CanvasManagerFactory(document.getElementById("gameCanvas"));
+
+    mapLoader = new MapLoader(resourceLoader);
+
     function loadedMap(resolve, reject) {
-        mapLoader.on(MAPS_READY_EVENT, function() {
+        mapLoader.on(MAPS_READY_EVENT, function () {
             mapRenderer = mapLoader.getMapRenderer();
             player.setMapRenderer(mapRenderer);
             mapRenderer.showCollisions();
             resolve();
         });
     }
-    
-    
-    
+
+
+
     // push map loading to pseudo-semaphore so we wait on all the maps
     loadedPromisesArr.push(promisify(loadedMap));
     player = new Player(canvasManager.canvas, loadedPromisesArr);
-	// initialise the movementManager with the player reference
-	movementManager = new MovementManager(player);
-    
+    // initialise the movementManager with the player reference
+    movementManager = new MovementManager(player);
+
     // wait on loadMap and loadPlayer
     waitOnAllPromises(loadedPromisesArr).then(
         function onResolved() {
@@ -74,14 +76,14 @@ function init() {
     );
 
     mapLoader.load();
-    var dialogue = new DialogueBox();
-    dialogue.setQuestion("ce faci?");
+    var dialogue = new YesNoBox();
+    dialogue.setQuestion("Ce faci?");
     dialogue.setOptions("bine", "bine", "foarte bine", "se lipesc banii de mine");
+    dialogue.waitOnInput();
 }
 
 function draw() {
-	mapRenderer.draw();
-	player.draw();
-	requestAnimationFrame(draw);
+    mapRenderer.draw();
+    player.draw();
+    requestAnimationFrame(draw);
 }
-
