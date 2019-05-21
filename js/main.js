ced59312af2974e1590efbe7e49ee2f3d61273ea
Mihai Pure <39124927+MihaiPure@users.noreply.github.com>
2019-05-21 13:31:06 +0300

    beautified code

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 let DEBUGGING = true;
 
 MapLoader.RESOURCES = [
-    {
-        name: "MainMap",
-        itemType: "JSON",
-        url: "Tiled/map/map1.json"
+	{
+		name: "MainMap",
+		itemType: "JSON",
+		url: "Tiled/map/map1.json"
 	},
-    {
-        name: "CastleMap",
-        itemType: "JSON",
-        url: "Tiled/map/castle2.json"
+	{
+		name: "CastleMap",
+		itemType: "JSON",
+		url: "Tiled/map/castle2.json"
 	},
-    {
-        name: "Dungeon",
-        itemType: "JSON",
-        url: "Tiled/map/dungeon.json"
+	{
+		name: "Dungeon",
+		itemType: "JSON",
+		url: "Tiled/map/dungeon.json"
     },
 ];
 
 // in case the name of the map resources changes
 const MAIN_MAP = "MainMap",
-    CASTLE_MAP = "CastleMap",
-    DUNGEON = "Dungeon";
+	CASTLE_MAP = "CastleMap",
+	DUNGEON = "Dungeon";
 
 /*
     when this event fires on the mapLoader instance 
@@ -39,71 +39,71 @@ let loadingScreen, resourceLoader, canvasManager, mapRenderer, mapLoader, player
 let DRAWABLE_ENTITIES = [];
 
 function init() {
-    loadingScreen = new LoadingScreen();
-	
+	loadingScreen = new LoadingScreen();
+
 	if (DEBUGGING) {
 		let stats = new xStats();
 		document.body.appendChild(stats.element);
 	}
-	
+
 	resourceLoader = new ResourceLoader();
 	canvasManager = CanvasManagerFactory(document.getElementById("gameCanvas"));
-	
+
 	mapLoader = new MapLoader(resourceLoader);
-	
-    function loadedMap(resolve, reject) {
-        mapLoader.on(MAPS_READY_EVENT, function () {
-            mapRenderer = mapLoader.getMapRenderer();
-            //mapRenderer.showCollisions();
+
+	function loadedMap(resolve, reject) {
+		mapLoader.on(MAPS_READY_EVENT, function () {
+			mapRenderer = mapLoader.getMapRenderer();
+			//mapRenderer.showCollisions();
 			// setting the mapRenderer
 			StateSaverManager().setMapRenderer(mapRenderer);
-			
-            resolve();
-        });
-	}
-					 
-    // push map loading to pseudo-semaphore so we wait on all the maps
-    loadedPromisesArr.push(promisify(loadedMap));
 
-    // Load images for Actor weapons and arrow Projectiles
+			resolve();
+		});
+	}
+
+	// push map loading to pseudo-semaphore so we wait on all the maps
+	loadedPromisesArr.push(promisify(loadedMap));
+
+	// Load images for Actor weapons and arrow Projectiles
 	loadWeaponsResources();
 	loadProjectileResources();
-	
-    player = new Player(loadedPromisesArr, {
-		"base" : "playerBody1",
-		"hair" : null,
-		"feetArmour" : "pantsArmour1",
-		"bootsArmour" : "bootsArmour1",
-		"bodyArmour" : "bodyArmour1",
-		"armsArmour" : "armsArmour1",
-		"headArmour" : "helmArmour1"
+
+	player = new Player(loadedPromisesArr, {
+		"base": "playerBody1",
+		"hair": null,
+		"feetArmour": "pantsArmour1",
+		"bootsArmour": "bootsArmour1",
+		"bodyArmour": "bodyArmour1",
+		"armsArmour": "armsArmour1",
+		"headArmour": "helmArmour1"
 	}, Actor.SPEAR);
-	
+
 	// setting the stateSaver in the mapLoader and also initialising it
 	mapLoader.setStateSaver(StateSaverManager());
 	// passing the reference to the player
 	mapLoader.setPlayerReference(player);
-	
+
 	// initialise the movementManager with the player reference
 	movementManager = new MovementManager(player);
 
-    // push map loading to pseudo-semaphore so we wait on all the maps
-    loadedPromisesArr.push(promisify(loadedMap));
+	// push map loading to pseudo-semaphore so we wait on all the maps
+	loadedPromisesArr.push(promisify(loadedMap));
 
-    // initialise the movementManager with the player reference
-    movementManager = new MovementManager(player);
+	// initialise the movementManager with the player reference
+	movementManager = new MovementManager(player);
 
-    // wait on loadMap and loadPlayer
-    waitOnAllPromises(loadedPromisesArr).then(
-        function onResolved() {
-            initGameOnLoaded();
-        },
-        function onRejected(err) {
-            console.error(err);
-        }
-    );
+	// wait on loadMap and loadPlayer
+	waitOnAllPromises(loadedPromisesArr).then(
+		function onResolved() {
+			initGameOnLoaded();
+		},
+		function onRejected(err) {
+			console.error(err);
+		}
+	);
 
-    mapLoader.load();
+	mapLoader.load();
 }
 
 function loadWeaponsResources() {
@@ -114,16 +114,16 @@ function loadWeaponsResources() {
 				return idx;
 			}
 		}
-			
+
 		return -1;
 	}
-	
+
 	let weaponsResourcesNames = ["bow", "arrow", "spear", "dagger"],
 		weaponResources = [];
-	
+
 	// Actor.waitOn() static method needs a resLoader on the object it loads the resources for
 	Actor.WEAPONS.resLoader = new ResourceLoader();
-	
+
 	// load all the weapons images and add them in the Actor.WEAPONS dictionary
 	for (let resName of weaponsResourcesNames) {
 		// getting only the slice of weapons resources from all the Actor resources
@@ -131,7 +131,7 @@ function loadWeaponsResources() {
 		// waiting on the resource to load globally, also adding them to Actor.WEAPONS
 		Actor.waitOn(Actor.WEAPONS, loadedPromisesArr, resName);
 	}
-	
+
 	/*
 		Start loading the resources
 	 */
@@ -158,9 +158,9 @@ function loadProjectileResources() {
 }
 
 function initGameOnLoaded() {
-    // begin drawing everything
-    requestAnimationFrame(draw);
-    loadingScreen.removeLoadingScreen();
+	// begin drawing everything
+	requestAnimationFrame(draw);
+	loadingScreen.removeLoadingScreen();
 }
 
 function draw() {
@@ -171,17 +171,17 @@ function draw() {
 		// (i.e. they despawned, died, collided, etc.) or -1 if they are still in use
 		let index = entity.update();
 
-		if (index != -1 && typeof(index) === "number") {
+		if (index != -1 && typeof (index) === "number") {
 			DRAWABLE_ENTITIES.splice(index, 1);
 		}
 	}
-	
+
 	mapRenderer.draw();
 	player.draw();
 
 	for (let entity of DRAWABLE_ENTITIES) {
 		entity.draw();
 	}
-	
+
 	requestAnimationFrame(draw);
 }
--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,16 @@
-init_menu = function(){
-    
+init_menu = function () {
+
     let list_items = document.getElementsByClassName("option");
-    for(let i = 0; i < list_items.length; i++){
+    for (let i = 0; i < list_items.length; i++) {
         console.log(i);
-        list_items[i].addEventListener("mouseover", function(){
+        list_items[i].addEventListener("mouseover", function () {
             list_items[i].classList.add("hovered");
-            list_items[i].addEventListener("mouseout", function(){
+            list_items[i].addEventListener("mouseout", function () {
                 list_items[i].classList.remove("hovered");
             })
         })
     }
-    
-    
-    
+
+
+
 }
