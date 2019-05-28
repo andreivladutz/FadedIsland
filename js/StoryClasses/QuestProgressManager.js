fb5ac66c889c09d544c86bf1ea8d33bb87b48218
Tudiman <39316639+Tudiman@users.noreply.github.com>
2019-05-28 22:41:19 +0300

    sal2

--- js/StoryClasses/QuestProgressManager.js
+++ js/StoryClasses/QuestProgressManager.js
@@ -1,10 +1,24 @@
-class QuestProgressManager {
+class QuestProgressManager extends EventTarget{
 
-    constructor(monsters, quantity) {
+    constructor(monsters, progress, quantity) {
+
+        super();
 
         this.monsters = monsters;
+        this.progress = progress;
         this.quantity = quantity;
-        
+
+        let obj = this;
+        this.addEventListener("killedEnemy",function(name) {
+
+            console.log("sal");
+            if(obj.monsters.includes(name)) {
+                obj.progress++;
+            }
+            if(obj.progress === obj.quantity)
+                obj.removeEventListener("killedEnemy");
+
+        })
     }
 
 }
